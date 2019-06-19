3587416268c940f1f16efbf803623c7211825308
Michael Davidsaver <mdavidsaver@gmail.com>
2019-06-19 11:30:02 -0700

    pva/client.h c++ lambda for monitor()

--- examples/monitorme.cpp
+++ examples/monitorme.cpp
@@ -4,11 +4,14 @@
  * in file LICENSE that is included with this distribution.
  */
 
+#if __cplusplus>=201103L
+
 #include <set>
 #include <queue>
 #include <vector>
 #include <string>
 #include <exception>
+#include <iostream>
 
 #if !defined(_WIN32)
 #include <signal.h>
@@ -19,6 +22,7 @@
 #include <epicsMutex.h>
 #include <epicsGuard.h>
 #include <epicsGetopt.h>
+#include <epicsAtomic.h>
 
 #include <pv/configuration.h>
 #include <pv/caProvider.h>
@@ -34,155 +38,17 @@ namespace {
 typedef epicsGuard<epicsMutex> Guard;
 typedef epicsGuardRelease<epicsMutex> UnGuard;
 
-struct Worker {
-    virtual ~Worker() {}
-    virtual void process(const pvac::MonitorEvent& event) =0;
-};
-
-// simple work queue with thread.
-// moves monitor queue handling off of PVA thread(s)
-struct WorkQueue : public epicsThreadRunable {
-    epicsMutex mutex;
-    typedef std::tr1::shared_ptr<Worker> value_type;
-    typedef std::tr1::weak_ptr<Worker> weak_type;
-    // work queue holds only weak_ptr
-    // so jobs must be kept alive seperately
-    typedef std::deque<std::pair<weak_type, pvac::MonitorEvent> > queue_t;
-    queue_t queue;
-    epicsEvent event;
-    bool running;
-    pvd::Thread worker;
-
-    WorkQueue()
-        :running(true)
-        ,worker(pvd::Thread::Config()
-                .name("Monitor handler")
-                .autostart(true)
-                .run(this))
-    {}
-    ~WorkQueue() {close();}
-
-    void close()
-    {
-        {
-            Guard G(mutex);
-            running = false;
-        }
-        event.signal();
-        worker.exitWait();
-    }
-
-    void push(const weak_type& cb, const pvac::MonitorEvent& evt)
-    {
-        bool wake;
-        {
-            Guard G(mutex);
-            if(!running) return; // silently refuse to queue during/after close()
-            wake = queue.empty();
-            queue.push_back(std::make_pair(cb, evt));
-        }
-        if(wake)
-            event.signal();
-    }
-
-    virtual void run() OVERRIDE FINAL
-    {
-        Guard G(mutex);
-
-        while(running) {
-            if(queue.empty()) {
-                UnGuard U(G);
-                event.wait();
-            } else {
-                queue_t::value_type ent(queue.front());
-                value_type cb(ent.first.lock());
-                queue.pop_front();
-                if(!cb) continue;
-
-                try {
-                    UnGuard U(G);
-                    cb->process(ent.second);
-                }catch(std::exception& e){
-                    std::cout<<"Error in monitor handler : "<<e.what()<<"\n";
-                }
-            }
-        }
-    }
-};
-
-WorkQueue monwork;
-epicsMutex mutex;
+// We want to gracefully cleanup on SIGINT
 epicsEvent done;
-volatile size_t waitingFor;
+int ret = 0;
 
 #ifdef USE_SIGNAL
 void sigdone(int num)
 {
     (void)num;
-    waitingFor = 0;
     done.signal();
 }
 #endif
-
-struct MonTracker : public pvac::ClientChannel::MonitorCallback,
-                    public Worker,
-                    public std::tr1::enable_shared_from_this<MonTracker>
-{
-    POINTER_DEFINITIONS(MonTracker);
-
-    MonTracker(const std::string& name) :name(name) {}
-    virtual ~MonTracker() {mon.cancel();}
-
-    const std::string name;
-    pvac::Monitor mon;
-
-    virtual void monitorEvent(const pvac::MonitorEvent& evt) OVERRIDE FINAL
-    {
-        // shared_from_this() will fail as Cancel is delivered in our dtor.
-        if(evt.event==pvac::MonitorEvent::Cancel) return;
-
-        // running on internal provider worker thread
-        // minimize work here.
-        // TODO: bound queue size
-        monwork.push(shared_from_this(), evt);
-    }
-
-    virtual void process(const pvac::MonitorEvent& evt) OVERRIDE FINAL
-    {
-        // running on our worker thread
-        switch(evt.event) {
-        case pvac::MonitorEvent::Fail:
-            std::cout<<"Error "<<name<<" "<<evt.message<<"\n";
-            break;
-        case pvac::MonitorEvent::Cancel:
-            std::cout<<"Cancel "<<name<<"\n";
-            break;
-        case pvac::MonitorEvent::Disconnect:
-            std::cout<<"Disconnect "<<name<<"\n";
-            break;
-        case pvac::MonitorEvent::Data:
-        {
-            unsigned n;
-            for(n=0; n<2 && mon.poll(); n++) {
-                pvd::PVField::const_shared_pointer fld(mon.root->getSubField("value"));
-                if(!fld)
-                    fld = mon.root;
-
-                std::cout<<"Event "<<name<<" "<<fld
-                        <<" Changed:"<<mon.changed
-                       <<" overrun:"<<mon.overrun<<"\n";
-            }
-            if(n==2) {
-                // too many updates, re-queue to balance with others
-                monwork.push(shared_from_this(), evt);
-            } else if(n==0) {
-                std::cerr<<"Spurious Data event "<<name<<"\n";
-            }
-        }
-            break;
-        }
-    }
-};
 
 } // namespace
 
@@ -190,8 +56,10 @@ int main(int argc, char *argv[]) {
     try {
         epics::RefMonitor refmon;
         double waitTime = -1.0;
-        std::string providerName("pva"),
-                    requestStr("field()");
+        // default to native PVA client
+        std::string providerName("pva");
+        // default to "pvRequest" which asks for all fields
+        std::string requestStr("field()");
         typedef std::vector<std::string> pvs_t;
         pvs_t pvs;
 
@@ -228,67 +96,159 @@ int main(int argc, char *argv[]) {
         signal(SIGQUIT, sigdone);
 #endif
 
-        // build "pvRequest" which asks for all fields
         pvd::PVStructure::shared_pointer pvReq(pvd::createRequest(requestStr));
 
-        // explicitly select configuration from process environment
-        pva::Configuration::shared_pointer conf(pva::ConfigurationBuilder()
-                                                .push_env()
-                                                .build());
-
         // "pva" provider automatically in registry
-        // add "ca" provider to registry
+        // add "ca" provider to registry even when we won't use it.
         pva::ca::CAClientFactory::start();
 
-        std::cout<<"Use provider: "<<providerName<<"\n";
-        pvac::ClientProvider provider(providerName, conf);
+        // Note: With "pva" provider, could provide a Configuration as the second argument
+        //       to override default (EPICS_PVA_* environment variables).
+        //       Doesn't apply to "ca".
+        pvac::ClientProvider provider(providerName);
 
-        std::vector<MonTracker::shared_pointer> monitors;
+        size_t remaining = pvs.size();
 
-        {
-            Guard G(mutex);
-            waitingFor = pvs.size();
+        // the ordering of the following is critical for a safe shutdown.
+        // first created is last destroyed
+
+        bool clockoff = false;
+        epicsEvent ready;
+        epicsMutex mutex;
+
+        // work queue
+        std::deque<pvac::MonitorEvent> work;
+
+        // holder for subscriptions.
+        // subscriptions are implicitly closed when Monitor's are destroyed (when 'subs' goes out of scope)
+        std::vector<pvac::Monitor> subs;
+        subs.reserve(pvs.size());
+
+        // Subscribe to all requested PVs, and feed updates into our work queue.
+        // Will do I/O on our own worker thread instead of the PVA worker threads.
+        for(const auto& pv : pvs) {
+            subs.push_back(provider
+                           .connect(pv)
+                           .monitor([&work, &ready, &mutex](const pvac::MonitorEvent& evt)
+            {
+                // Callback on a shared PVA worker thread.
+                bool poke;
+                {
+                    Guard G(mutex);
+                    poke = work.empty();
+                    work.push_back(evt);
+                }
+                // signal when the work queue is no longer empty
+                if(poke)
+                    ready.signal();
+            }));
         }
 
-        for(pvs_t::const_iterator it=pvs.begin(); it!=pvs.end(); ++it) {
-            const std::string& pv = *it;
-
-            MonTracker::shared_pointer mon(new MonTracker(pv));
-
-            pvac::ClientChannel chan(provider.connect(pv));
-
-            mon->mon = chan.monitor(mon.get(), pvReq);
-
-            monitors.push_back(mon);
-        }
-
-        int ret = 0;
+        // Start up our worker thread
+        pvd::Thread worker(pvd::Thread::Config()
+                           .name("worker")
+                           .autostart(true)
+                           .run([&work, &ready, &mutex, &remaining, &clockoff]()
         {
+            // our worker thread run()
             Guard G(mutex);
-            while(waitingFor) {
-                UnGuard U(G);
-                if(waitTime<0.0) {
-                    done.wait();
-                } else if(!done.wait(waitTime)) {
-                    std::cerr<<"Timeout\n";
-                    ret = 1;
-                    break; // timeout
+            while(!clockoff) {
+                // wait for the queue to become not-empty
+                {
+                    UnGuard U(G);
+                    ready.wait();
+                }
+                // loop until the queue is drained
+                while(!work.empty()) {
+                    const pvac::MonitorEvent evt = work.front();
+                    work.pop_front();
+                    bool requeue = false;
+
+                    pvac::Monitor mon(evt.monitor());
+
+                    {
+                        // MonitorEvent is a copy, and may be handled w/o lockingn
+                        UnGuard U(G);
+
+                        auto name = mon.name();
+
+                        switch(evt.event) {
+                        case pvac::MonitorEvent::Fail:
+                            std::cout<<name<<" Error : "<<evt.message<<"\n";
+                            epics::atomic::set(ret, 1);
+                            break;
+                        case pvac::MonitorEvent::Cancel:
+                            std::cout<<name<<" Cancel\n";
+                            break;
+                        case pvac::MonitorEvent::Disconnect:
+                            std::cout<<name<<" Disconnect\n";
+                            break;
+                        case pvac::MonitorEvent::Data:
+                        {
+                            unsigned n;
+                            for(n=0; n<2 && mon.poll(); n++) {
+                                pvd::PVField::const_shared_pointer fld(mon.root->getSubField("value"));
+                                if(!fld)
+                                    fld = mon.root;
+
+                                std::cout<<"Event "<<name<<" "<<fld
+                                         <<" Changed:"<<mon.changed
+                                         <<" overrun:"<<mon.overrun<<"\n";
+                            }
+                            if(n==2) {
+                                // too many updates, re-queue to balance with others
+                                requeue = true;
+                                break;
+                            } else if(n==0) {
+                                std::cerr<<"Spurious Data event "<<name<<"\n";
+                            }
+                        }
+                            break;
+                        }
+                    }
+
+                    if(requeue)
+                        work.push_back(evt);
+
+                    if(mon.complete() && epics::atomic::decrement(remaining)==0)
+                        done.signal();
                 }
             }
-        }
 
-        if(refmon.running()) {
-            refmon.stop();
-            // drop refs to operations, but keep ref to ClientProvider
-            monitors.clear();
-            // show final counts
-            refmon.current();
-        }
+        }));
+        // now need to make sure the worker is interruptedon success or error
 
-        monwork.close();
+        try {
+            if(waitTime<0.0) {
+                done.wait();
+
+            } else if(!done.wait(waitTime)) {
+                std::cerr<<"Timeout\n";
+                epics::atomic::set(ret, 1);
+            }
+
+        }catch(...){
+            clockoff = true;
+            ready.signal();
+            throw;
+        }
+        clockoff = true;
+        ready.signal();
+
         return ret;
+
     } catch(std::exception& e){
         std::cout<<"Error: "<<e.what()<<"\n";
         return 2;
     }
 }
+
+#else // ! __cplusplus>=201103L
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cerr<<"This Example must be built with c++11 enabled\n";
+    return 1;
+}
+#endif // __cplusplus>=201103L
