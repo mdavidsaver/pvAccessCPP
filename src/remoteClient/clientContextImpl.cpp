eb65148229da0982fa988125a0ae40d8ae85c710
Michael Davidsaver <mdavidsaver@gmail.com>
2019-07-08 10:56:43 -0700

    minor
    disambiguate error messages

--- src/remoteClient/clientContextImpl.cpp
+++ src/remoteClient/clientContextImpl.cpp
@@ -4015,7 +4015,7 @@ public:
         Lock lock(m_contextMutex);
 
         if (m_contextState == CONTEXT_DESTROYED)
-            throw std::runtime_error("Context destroyed.");
+            throw std::runtime_error("Context destroyed!");
         else if (m_contextState == CONTEXT_INITIALIZED)
             throw std::runtime_error("Context already initialized.");
 
